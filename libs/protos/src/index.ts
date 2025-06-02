34e29ba1709c8ecffb03c0eee344b420dc55c344
bcila <b.cila51@gmail.com>
2025-06-02 14:05:37 +0300

    chore: better exports in protos index

--- libs/protos/src/index.ts
+++ libs/protos/src/index.ts
@@ -1,6 +1,14 @@
+import path from 'path';
+
 export * from './lib/protos.module';
 
+export * as AUTH from './generated/auth';
+export * as STREAM from './generated/stream';
+export * as USER from './generated/user';
+
 // PROTO FILES
-export const AUTH_PROTO_PATH = __dirname + '/proto/auth.proto';
-export const STREAM_PROTO_PATH = __dirname + '/proto/stream.proto';
-export const USER_PROTO_PATH = __dirname + '/proto/user.proto';
+const protoBasePath = path.join(__dirname, 'proto');
+
+export const AUTH_PROTO_PATH = path.join(protoBasePath, 'auth.proto');
+export const STREAM_PROTO_PATH = path.join(protoBasePath, 'stream.proto');
+export const USER_PROTO_PATH = path.join(protoBasePath, 'user.proto');
